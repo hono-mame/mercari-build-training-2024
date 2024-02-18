7e896f08e7e5d35d11f3400428c6ab2539a4ee7f
Honoka Kobayashi <honoka.kobayashi@rhelixa.com>
2024-02-19 00:41:39 +0900

    make an endpoint to add a new item

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"fmt"
 	"net/http"
 	"os"
@@ -14,7 +16,17 @@ import (
 
 const (
 	ImgDir = "images"
+	JSONFile = "items.json"
 )
+
+type Item struct {
+	Name string `json:"name"`
+	Category string `json:"category"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
+}
 
 type Response struct {
 	Message string `json:"message"`
@@ -25,19 +37,65 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func readItemsFromFile() (*Items, error) {
+	file, err := ioutil.ReadFile(JSONFile)
+	if err != nil {
+		return nil, err
+	}
+	var items Items
+	// storing file contents in items
+	// JSON -> Items
+	if err := json.Unmarshal(file, &items); err != nil {
+		return nil, err
+	}
+	return &items, nil
+}
+
+func writeItemsToFile(items *Items) error {
+    file, err := os.Create(JSONFile)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+	// Items -> JSON
+    encoder := json.NewEncoder(file)
+    if err := encoder.Encode(items); err != nil {
+        return err
+    }
+    return nil
+}
+
+
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
-
-	message := fmt.Sprintf("item received: %s", name)
+	category := c.FormValue("category")
+	// Check if name or category is empty
+	if name == "" || category == "" {
+		return c.JSON(http.StatusBadRequest, 
+			Response{Message: "Name or category cannot be empty"})
+	}
+	// Create new item to add to JSON file
+	new_item := Item{Name: name, Category: category}
+	// for debug: Received item
+	fmt.Printf("Received item: %+v\n", new_item)
+	// Read existing items from JSON file
+	items, err := readItemsFromFile()
+	if err != nil {
+		return err
+	}
+	// Append new item to items
+	items.Items = append(items.Items, new_item)
+	// Write items back to JSON file
+	if err := writeItemsToFile(items); err != nil {
+		return err
+	}
+	message := fmt.Sprintf("Item received: %s", new_item.Name)
 	res := Response{Message: message}
-
 	return c.JSON(http.StatusOK, res)
 }
 
 func getImg(c echo.Context) error {
-	// Create image path
 	imgPath := path.Join(ImgDir, c.Param("imageFilename"))
 
 	if !strings.HasSuffix(imgPath, ".jpg") {
@@ -54,7 +112,6 @@ func getImg(c echo.Context) error {
 func main() {
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Logger.SetLevel(log.INFO)
@@ -68,12 +125,9 @@ func main() {
 		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 	}))
 
-	// Routes
 	e.GET("/", root)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
 
-
-	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
 }
